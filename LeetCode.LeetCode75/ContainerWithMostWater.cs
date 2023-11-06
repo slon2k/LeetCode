f47f925d36900a89a3eaed26eca837b5ca012eae
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-06 12:27:14 +0600

    problem solution updates 12/75

---
+++ LeetCode.LeetCode75/ContainerWithMostWater.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int length = height.Length;
+
+            int left = 0, right = length - 1;
+
+            int maxArea = 0;
+
+            while (left < right)
+            {
+                int area = Area(height, left, right);
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+
+        private int Area(int[] a, int left, int right)
+        {
+            int height = a[left] < a[right] ? a[left] : a[right];
+            return height * (right - left);
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/ContainerWithMostWaterTests.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class ContainerWithMostWaterTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+    public void MaxAreaTest(int[] data, int expected)
+    {
+        var solution = new ContainerWithMostWater.Solution();
+
+        var result = solution.MaxArea(data);
+
+        Assert.Equal(expected, result);
+    }
+}
