b281c980f8f5dd9ccd2a1419684a91642484886d
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-06 13:44:44 +0600

    problem solution updates 13/75

---
+++ LeetCode.LeetCode75/MaxNumberKSumPairs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Immutable;
+
+namespace LeetCode.LeetCode75;
+
+public class MaxNumberKSumPairs
+{
+    public class Solution
+    {
+        public int MaxOperations(int[] numbers, int k)
+        {
+            int count = 0;
+
+            Array.Sort(numbers);
+
+            int left = 0, right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] == k)
+                {
+                    count++;
+                    left++;
+                    right--;
+
+                    continue;
+                }
+
+                if (numbers[left] + numbers[right] < k)
+                {
+                    left++;
+
+                    continue;
+                }
+
+                right--;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/MaxNumberKSumPairsTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class MaxNumberKSumPairsTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 5, 2)]
+    [InlineData(new int[] { 3, 1, 3, 4, 3 }, 6, 1)]
+    public void MaxOperationsTest(int[] numbers, int k, int expected)
+    {
+        var solution = new MaxNumberKSumPairs.Solution();
+
+        var result = solution.MaxOperations(numbers, k);
+
+        Assert.Equal(expected, result);
+    }
+}
