382477cd5713b5907d1658c095eda87cc3c29930
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-06 22:59:39 +0600

    problem solution updates 16/75

---
+++ LeetCode.LeetCode75/MaxConsecutiveOnesIII.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace LeetCode.LeetCode75;
+
+public class MaxConsecutiveOnesIII
+{
+    public class Solution
+    {
+        public int LongestOnes(int[] nums, int k)
+        {
+            int left = 0, right = 0;
+
+            int count = k;
+
+            int length = 0;
+
+            int maxLength = 0;
+
+            while(right < nums.Length)
+            {
+                if (nums[right] == 1)
+                {
+                    right++;
+                    length++;
+                    maxLength = length > maxLength ? length : maxLength;
+
+                    continue;
+                }
+
+                if (count > 0)
+                {
+                    right++;
+                    count--;
+                    length++;
+
+                    maxLength = length > maxLength ? length : maxLength;
+
+                    continue;
+                }
+                
+                if (right + 1 == nums.Length)
+                {
+                    break;
+                }
+
+                if (left == right)
+                {
+                    left++;
+                    right++;
+
+                    continue;
+                }
+
+                if (nums[left] == 0)
+                {
+                    left++;
+
+                    if (count < k) 
+                    {
+                        count++;
+                    }
+
+                    length--;
+
+                    continue;
+                }
+
+                left++;
+                length--;
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/MaxConsecutiveOnesIIITests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class MaxConsecutiveOnesIIITests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 2, 6)]
+    [InlineData(new int[] { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, 3, 10)]
+    [InlineData(new int[] { 0, 0, 1, 1, 1, 0, 0 }, 0, 3)]
+    public void LongestOnesTest(int[] nums, int k, int expected)
+    {
+        var solution = new MaxConsecutiveOnesIII.Solution();
+
+        var result = solution.LongestOnes(nums, k);
+
+        Assert.Equal(expected, result);
+    }
+}
