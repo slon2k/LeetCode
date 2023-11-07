fabbbe2176fdc48406bd16b2609be80133ee51e4
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-07 19:26:56 +0600

    problem 21

---
+++ LeetCode.LeetCode75/UniqueNumberOfOccurrences.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class UniqueNumberOfOccurrences
+{
+    public class Solution
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> occurrences = new Dictionary<int, int>();
+
+            var occurrencesSet = new HashSet<int>();
+
+            foreach (int item in arr)
+            {
+                if (occurrences.ContainsKey(item)) 
+                {
+                    occurrences[item]++;
+                } else
+                {
+                    occurrences[item] = 1;
+                }
+            }
+
+            foreach (int item in occurrences.Values)
+            {
+                if (!occurrencesSet.Contains(item))
+                {
+                    occurrencesSet.Add(item);
+                } else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/UniqueNumberOfOccurrencesTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class UniqueNumberOfOccurrencesTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 2, 1, 1, 3 }, true)]
+    [InlineData(new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 }, true)]
+    [InlineData(new int[] { 1, 2 }, false)]
+    public void UniqueOccurrencesTest(int[] data, bool expected)
+    {
+        var solution = new UniqueNumberOfOccurrences.Solution();
+
+        var result = solution.UniqueOccurrences(data);
+
+        Assert.Equal(expected, result);
+    }
+}
