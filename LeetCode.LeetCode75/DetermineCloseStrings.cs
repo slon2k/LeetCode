9d4d398aef745b43c5721487f675466b8c59bd49
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-08 10:03:14 +0600

    problem 22

---
+++ LeetCode.LeetCode75/DetermineCloseStrings.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class DetermineCloseStrings
+{
+    public class Solution
+    {
+        public bool CloseStrings(string word1, string word2)
+        {
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+
+            var occurrences1 = GetOccurrences(word1);
+
+            var occurrences2 = GetOccurrences(word2);
+
+
+            if (!AreEqual(occurrences1.Keys.ToArray(), occurrences2.Keys.ToArray()) || !AreEqual(occurrences1.Values.ToArray(), occurrences2.Values.ToArray())) 
+            { 
+                return false;
+            }
+
+            return true;
+
+        }
+
+        private static Dictionary<char, int> GetOccurrences(string word)
+        {
+            var occurrences = new Dictionary<char, int>();
+
+            foreach (var c in word)
+            {
+                if (!occurrences.ContainsKey(c))
+                {
+                    occurrences.Add(c, 1);
+                } else 
+                {
+                    occurrences[c]++;
+                }
+            }
+
+            return occurrences;
+        }
+
+        private static bool AreEqual<T>(T[] list1, T[] list2)
+        {
+            if (list1.Length != list2.Length)
+            {
+                return false;
+            }
+
+            Array.Sort(list1);
+            Array.Sort(list2);
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                if (!list1[i]?.Equals(list2[i]) ?? false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/DetermineCloseStringsTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class DetermineCloseStringsTests
+{
+    public class SolutionTest
+    {
+
+        [Theory]
+        [InlineData("abc", "bca", true)]
+        [InlineData("a", "aa", false)]
+        [InlineData("cabbba", "abbccc", true)]
+        [InlineData("cabbba", "aabbcc", false)]
+        [InlineData("aaabbbbccddeeeeefffff", "aaaaabbcccdddeeeeffff", false)]
+        public void CloseStringsTest(string word1, string word2, bool expected)
+        {
+            var solution = new DetermineCloseStrings.Solution();
+
+            var result = solution.CloseStrings(word1, word2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
