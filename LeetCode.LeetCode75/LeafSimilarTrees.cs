1137cbc21bfbc388e7f61c83f621741c1f9fa22a
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-10 09:40:31 +0600

    problems 34/75

---
+++ LeetCode.LeetCode75/LeafSimilarTrees.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class LeafSimilarTrees
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            return SequenceEqual(GetSequence(root1), GetSequence(root2));
+        }
+
+        private List<int> GetSequence(TreeNode root)
+        {
+            var sequence = new List<int>();
+
+            Traverse(root, sequence);
+
+            return sequence;
+        }
+
+        private void Traverse(TreeNode? node, List<int> sequence)
+        {
+            if (node is null)
+            {
+                return;
+            }
+
+            if(node.left is null && node.right is null)
+            {
+                sequence.Add(node.val);
+                return;
+            }
+            
+            Traverse(node.left, sequence);
+            Traverse(node.right, sequence);
+        }
+
+        private bool SequenceEqual(List<int> list1, List<int> list2)
+        {
+            if (list1.Count != list2.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i] != list2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75/MaximumDepthBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class MaximumDepthBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return GetDepth(root);
+        }
+
+        private int GetDepth(TreeNode node)
+        {
+            if (node is null) 
+                return 0;
+
+            return Math.Max(GetDepth(node.left), GetDepth(node.right)) + 1;
+
+        }
+    }
+}
