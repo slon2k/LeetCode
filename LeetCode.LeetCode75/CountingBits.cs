97bdd580126a7089b38bade73d745bfa1fd6f052
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-11 22:31:45 +0600

    problem 43

---
+++ LeetCode.LeetCode75/CountingBits.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class CountingBits
+{
+    public class Solution
+    {
+        public int[] CountBits(int n)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i <= n; i++)
+            {
+                list.Add(Count(i));
+            }
+
+            return list.ToArray();
+        }
+
+        private int Count(int n)
+        {
+            int count = 0;
+            
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    count++;
+                }
+
+                n /= 2;
+            }
+
+            return count;
+        }
+    }
+}
