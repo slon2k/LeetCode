bc063d4a82cd763fac46ec86a503ce807c3556a8
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-11 16:31:16 +0600

    problem 42

--- LeetCode.LeetCode75/DeleteNodeBST.cs
+++ LeetCode.LeetCode75/DeleteNodeBST.cs
@@ -19,7 +19,77 @@ internal class DeleteNodeBST
     {
         public TreeNode DeleteNode(TreeNode root, int key)
         {
-            throw new NotImplementedException();
+            if (root is null)
+            {
+                return root;
+            }
+
+            if (root.val == key)
+            {
+                return DeleteNode(root);
+            }
+
+            if (root.val > key)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+
+            if (root.val < key)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+
+            return root;
+        }
+
+        private TreeNode? DeleteNode(TreeNode? node)
+        {
+            if (node is null)
+            {
+                return null;
+            }
+
+            if (node.left is null)
+            {
+                return node.right;
+            }
+
+            if (node.right is null)
+            {
+                return node.left;
+            }
+
+            int max = FindMax(node.left);
+
+            node.val = max;
+
+            node.left = DeleteNode(node.left, max);
+
+            return node;
+        }
+
+        private int FindMax(TreeNode root)
+        {
+            var node = root;
+
+            while (node.right is not null)
+            {
+                node = node.right;
+            }
+
+            return node.val;
+        }
+
+        private int FindMin(TreeNode root)
+        {
+            var node = root;
+
+            while (node.left is not null)
+            { 
+                node = node.left; 
+            }
+
+            return node.val;
         }
     }
 }
