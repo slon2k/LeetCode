ff03d3d23cbcb087fc530269a86ba89ac843e2e1
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-13 16:47:19 +0600

    problem 48

---
+++ LeetCode.LeetCode75/ReorderRoutesToCityZero.cs
@@ -0,0 +1,66 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class ReorderRoutesToCityZero
+{
+    public class Solution
+    {
+        public int MinReorder(int n, int[][] connections)
+        {
+            List<Connection>[] roads = new List<Connection>[n];
+            int[] visited = new int[n];
+            int[] previous = new int[n];
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                roads[i] = new List<Connection>();
+            }
+
+            for (int i = 0; i < connections.Length; i++)
+            {
+                int first = connections[i][0];
+                int second = connections[i][1];
+
+                var road = new Connection(first, second);
+                
+                roads[first].Add(road);
+                roads[second].Add(road);
+            }
+
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(0);
+
+            while (stack.Count > 0)
+            {
+                int city = stack.Pop();
+
+                if (visited[city] == 0)
+                {
+                    visited[city] = 1;
+
+                    if (roads[city].Contains(new Connection(previous[city], city)))
+                    {
+                        count++;
+                    }
+
+                    foreach (var road in roads[city])
+                    {
+                        int next = road.First != city ? road.First : road.Second;
+
+                        if (visited[next] == 0) 
+                        {
+                            previous[next] = city;
+
+                            stack.Push(next);
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public record Connection(int First, int Second);
+    }
+}
---
+++ LeetCode.LeetCode75Tests/ReorderRoutesToCityZeroTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class ReorderRoutesToCityZeroTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(6, new string[] { "0,1", "1,3", "2,3", "4,0", "4,5" },3)]
+        [InlineData(5, new string[] { "1,0", "1,2", "3,2", "3,4" }, 2)]
+        [InlineData(3, new string[] { "1,0", "2,0" }, 2)]
+        public void MinReorderTest(int n, string[] connections, int expected)
+        {
+            var solution = new ReorderRoutesToCityZero.Solution();
+
+            int[][] array = new int[connections.Length][];
+            
+            for (int i = 0; i < connections.Length; i++)
+            {
+                array[i] = connections[i].Split(',').Select(int.Parse).ToArray();
+            }
+
+            var result = solution.MinReorder(n, array);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
