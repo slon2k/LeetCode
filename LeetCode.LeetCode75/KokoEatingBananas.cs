14a86dcc5dd320a3254dc5e9e1126304f94f4be8
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-15 13:01:46 +0600

    problem 52

---
+++ LeetCode.LeetCode75/KokoEatingBananas.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class KokoEatingBananas
+{
+    public class Solution
+    {
+        public int MinEatingSpeed(int[] piles, int h)
+        {
+            long left = 1;
+            long right = int.MaxValue;
+
+            if (Hours(piles, left) <= h)
+            {
+                return 1;
+            }
+
+            while (left < right - 1)
+            {
+                long middle = left + (right - left)/2;
+
+                if (Hours(piles, middle) > h)
+                {
+                    left = middle;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return (int)left + 1;
+        }
+
+        private long Hours(int[] piles, long speed)
+        {
+            return piles.Select(x => x / speed + (x % speed > 0 ? 1 : 0)).Sum();
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/KokoEatingBananas.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class KokoEatingBananasTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 6, 7, 11 }, 8, 4)]
+        [InlineData(new int[] { 30, 11, 23, 4, 20 }, 5, 30)]
+        [InlineData(new int[] { 312884470 }, 968709470, 1)]
+        [InlineData(new int[] { 332484035, 524908576, 855865114, 632922376, 222257295, 690155293, 112677673, 679580077, 337406589, 290818316, 877337160, 901728858, 679284947, 688210097, 692137887, 718203285, 629455728, 941802184 }, 823855818, 14)]
+        public void MinEatingSpeedTest(int[] piles, int h, int expected)
+        {
+            var solution = new KokoEatingBananas.Solution();
+
+            var result = solution.MinEatingSpeed(piles, h);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
