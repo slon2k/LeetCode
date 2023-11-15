7f8f548745ee34a604f28fa05afb87b070400e0e
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-15 10:59:03 +0600

    problem 51

---
+++ LeetCode.LeetCode75/SuccessfulPairsSpellsPotions.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class SuccessfulPairsSpellsPotions
+{
+    public class Solution
+    {
+        public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {
+            Array.Sort(potions);
+
+            return spells.Select(s => BinarySearch(s, potions, success)).ToArray();
+        }
+
+        private int BinarySearch( int value, int[] source, long success)
+        {
+            int length = source.Length;
+
+            if (source[length - 1] * value < success) 
+            {
+                return 0;
+            }
+
+            if (source[0] * value >= success)
+            {
+                return length;
+            }
+
+            int left = 0;
+            int right = length - 1;
+
+            while (left < right - 1)
+            {
+                int middle = left + (right - left) / 2;
+
+                if (source[middle] * value < success)
+                {
+                    left = middle;
+                } else
+                {
+                    right = middle;
+                }
+            }
+
+            return length - 1 - left;
+        } 
+
+    }
+}
---
+++ LeetCode.LeetCode75Tests/SuccessfulPairsSpellsPotions.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class SuccessfulPairsSpellsPotionsTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 5, 1, 3 }, new int[] { 1, 2, 3, 4, 5 }, 7, new int[] { 4, 0, 3 })]
+        [InlineData(new int[] { 3, 1, 2 }, new int[] { 8, 5, 8 }, 16, new int[] { 2, 0, 2 })]
+        public void SuccessfulPairsTest(int[] spells, int[] potions, long success, int[] expected)
+        {
+            var solution = new SuccessfulPairsSpellsPotions.Solution();
+
+            var result = solution.SuccessfulPairs(spells, potions, success);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
