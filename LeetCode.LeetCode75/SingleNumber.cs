eb00334da51eb4e09d02e58ecf99bda9a8b1a7d2
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-18 13:05:10 +0600

    problems 56/75

---
+++ LeetCode.LeetCode75/SingleNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class SingleNumber
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+
+            foreach (var item in nums)
+            {
+                result ^= item;                
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75/TribonacciNumber.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class TribonacciNumber
+{
+    public class Solution
+    {
+        public int Tribonacci(int n)
+        {
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(0);
+            stack.Push(1);
+            stack.Push(1);
+
+            for (int i = 3; i <= n; i++)
+            {
+                int n2 = stack.Pop();
+                int n1 = stack.Pop();
+                int n0 = stack.Pop();
+
+                int sum = n0 + n1 + n2;
+
+                stack.Push(n1);
+                stack.Push(n2);
+                stack.Push(sum);
+            }
+
+            return stack.Pop();
+        }
+    }
+}
