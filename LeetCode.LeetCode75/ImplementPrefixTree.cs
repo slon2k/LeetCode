c7ef419fdec5c6836e6362a65f46c042458c2e09
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-24 15:15:49 +0600

    problem 64

---
+++ LeetCode.LeetCode75/ImplementPrefixTree.cs
@@ -0,0 +1,93 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class ImplementPrefixTree
+{
+    public class Trie
+    {
+        private readonly TrieNode head;
+
+        public Trie()
+        {
+            head = new TrieNode();
+        }
+
+        public void Insert(string word)
+        {
+            var node = head;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                node = node.TryAddChild(word[i]);
+            }
+
+            node.SetEnding(true);
+        }
+
+        public bool Search(string word)
+        {
+            TrieNode? node = head;
+
+            foreach (var c in word)
+            {
+                node = node?.GetChild(c);
+
+                if (node is null)
+                {
+                    return false;
+                }
+            }
+
+            if (node.IsWordEnding)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            TrieNode? node = head;
+
+            foreach (var c in prefix)
+            {
+                node = node?.GetChild(c);
+
+                if (node is null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private class TrieNode
+        {
+            private bool isWordEnding;
+
+            private readonly Dictionary<char, TrieNode> children = new();
+
+            public TrieNode(bool isWordEnding = false)
+            {
+                this.isWordEnding = isWordEnding;
+            }
+
+            public TrieNode? GetChild(char c) => children.ContainsKey(c) ? children[c] : null;
+
+            public TrieNode TryAddChild(char c)
+            {
+                if (!children.ContainsKey(c))
+                {
+                    children[c] = new TrieNode();
+                }
+
+                return children[c];
+            }
+
+            public bool IsWordEnding => isWordEnding;
+
+            public void SetEnding(bool isWordEnding) => this.isWordEnding = isWordEnding;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/ImplementPrefixTreeTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class ImplementPrefixTreeTests
+{
+    [Theory]
+    [InlineData(new string[] {}, new string[] { }, new bool[] { })]
+    public void TreeSearchTest(string[] words, string[] search, bool[] expected)
+    {
+        var trie = new ImplementPrefixTree.Trie();
+
+        foreach (var word in words)
+        {
+            trie.Insert(word);
+        }
+
+        var results = new List<bool>();
+
+        for (var i = 0; i < search.Length; i++)
+        {
+            results.Add(trie.Search(search[i]));
+        }
+
+        Assert.Equal(expected, results);
+    }
+}
