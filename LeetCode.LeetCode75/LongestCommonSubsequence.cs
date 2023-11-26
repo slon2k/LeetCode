49e6b6650460bb896036e17d9f992c850370b256
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-26 14:07:47 +0600

    problem 66

---
+++ LeetCode.LeetCode75/LongestCommonSubsequence.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class LongestCommonSubsequence
+{
+    public class Solution
+    {
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            var (length1, length2) = (text1.Length, text2.Length);
+
+            int[,] result = new int[length1 + 1, length2 + 1];
+
+            for (int i = 1; i <= length1; i++)
+            {
+                for (int j = 1; j <= length2; j++)
+                {
+                    result[i, j] = Math.Max(result[i - 1, j], result[i, j - 1]);
+
+                    if (text1[i - 1] == text2[j - 1] && result[i - 1, j] >= result[i, j - 1])
+                    {
+                        result[i, j]++;
+                        result[i, j] -= result[i - 1, j] - result[i - 1, j - 1];
+                    }
+                }
+            }
+
+            return result[length1, length2];
+        }
+    }
+}
---
+++ LeetCode.LeetCode75Tests/LongestCommonSubsequenceTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.LeetCode75;
+
+namespace LeetCode.LeetCode75Tests;
+
+public class LongestCommonSubsequenceTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData("ace", "abcde", 3)]
+        [InlineData("abc", "abc", 3)]
+        [InlineData("abc", "def", 0)]
+        [InlineData("abacd", "tattacx", 3)]
+        [InlineData("bsbininm", "jmjkbkjkv", 1)]
+        public void LongestCommonSubsequenceTest(string text1, string text2, int expected)
+        {
+            var solution = new LongestCommonSubsequence.Solution();
+
+            var result = solution.LongestCommonSubsequence(text1, text2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
