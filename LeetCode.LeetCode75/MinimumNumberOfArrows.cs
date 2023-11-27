e000203bdd11902cd0816ec06e31393d86fcec69
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-11-27 13:48:30 +0600

    problems 67, 68

---
+++ LeetCode.LeetCode75/MinimumNumberOfArrows.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class MinimumNumberOfArrows
+{
+    public class Solution
+    {
+        public int FindMinArrowShots(int[][] points)
+        {
+            Array.Sort(points, (x, y) => x[1].CompareTo(y[1]));
+
+            if (points.Length == 0)
+            {
+                return 0;
+            }
+
+            var current = points[0];
+
+            int count = 1;
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                var balloon = points[i];
+
+                if (balloon[0] > current[1])
+                {
+                    count++;
+                    current = balloon;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ LeetCode.LeetCode75/NonOverlappingIntervals.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class NonOverlappingIntervals
+{
+    public class Solution
+    {
+        public int EraseOverlapIntervals(int[][] intervals)
+        {
+            Array.Sort(intervals, (x, y) => x[1] - y[1]);
+
+            int count = 0;
+
+            if (intervals.Length == 0)
+            {
+                return 0;
+            }
+
+            var current = intervals[0];
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                var interval = intervals[i];
+
+                if (interval[0] < current[1])
+                {
+                    count ++;
+                } else
+                {
+                    current = interval;
+                }
+            }
+
+            return count;
+        }
+    }
+}
