63b0e8e87707265076678f17726cbb00f9a893e3
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-12-07 12:15:41 +0600

    LeetCode 150, problem 1

---
+++ LeetCode.LeetCode150Tests/ArrayString/MergeSortedArrayTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.LeetCode150.ArrayString;
+
+namespace LeetCode.LeetCode150Tests.ArrayString;
+
+public class MergeSortedArrayTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 })]
+    [InlineData(new int[] { 1 }, 1, new int[] { }, 0, new int[] { 1 })]
+    [InlineData(new int[] { 0 }, 0, new int[] { 1 }, 1, new int[] { 1 })]
+    [InlineData(new int[] { 4, 0, 0, 0, 0, 0 }, 1, new int[] { 1, 2, 3, 5, 6 }, 5, new int[] { 1, 2, 3, 4, 5, 6 })]
+    public void MergeTest(int[] nums1, int m, int[] nums2, int n, int[] expected)
+    {
+        var solution = new MergeSortedArray.Solution();
+
+        solution.Merge(nums1, m, nums2, n);
+
+        Assert.Equal(expected, nums1);
+    }
+}
---
+++ LeetCode150/ArrayString/MergeSortedArray.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.LeetCode150.ArrayString;
+
+public class MergeSortedArray
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+
+            for (int i = 0; i < m; i++)
+            {
+                int k = n + m - 1 - i;
+                nums1[k] = nums1[k - n];
+            }
+
+
+            int left = n;
+            int right = 0;
+
+            int current = 0;
+
+            while (current < n + m)
+            {
+                if (left > m + n - 1)
+                {
+                    nums1[current++] = nums2[right++];
+                    continue;
+                }
+
+                if (right >= n)
+                {
+                    nums1[current++] = nums1[left++];
+                    continue;
+                }
+
+                if (nums1[left] <= nums2[right])
+                {
+                    nums1[current++] = nums1[left++];
+                }
+                else
+                {
+                    nums1[current++] = nums2[right++];
+                }
+            }
+        }
+    }
+}
