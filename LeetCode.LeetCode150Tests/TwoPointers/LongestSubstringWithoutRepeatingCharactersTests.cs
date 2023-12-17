28fd83f3cb2084ebee4a70352dc879110137bb1c
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-12-17 14:34:09 +0600

    problem 30

---
+++ LeetCode.LeetCode150Tests/TwoPointers/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.LeetCode150.SlidingWindow;
+
+namespace LeetCode.LeetCode150Tests.TwoPointers;
+
+public class LongestSubstringWithoutRepeatingCharactersTests
+{
+    [Theory]
+    [InlineData("abcabcbb", 3)]
+    [InlineData("bbbbb", 1)]
+    [InlineData("pwwkew", 3)]
+    [InlineData("p", 1)]
+    [InlineData(" ", 1)]
+    public void LengthOfLongestSubstring(string s, int expected)
+    {
+        var solution = new LongestSubstringWithoutRepeatingCharacters.Solution();
+
+        var result = solution.LengthOfLongestSubstring(s);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode150/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.LeetCode150.SlidingWindow;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            int left = 0;
+            int right = 0;
+            int maxLength = 1;
+            string current = s[0].ToString();
+
+            while (left < s.Length && right < s.Length)
+            {
+                right++;
+
+                if (right == s.Length)
+                {
+                    break;
+                }
+
+                while (current.Contains(s[right]))
+                {
+                    left++;
+                    current = current[1..];
+                }
+
+                current += s[right];
+                maxLength = Math.Max(maxLength, current.Length);
+            }
+
+            return maxLength;
+        }
+    }
+}
