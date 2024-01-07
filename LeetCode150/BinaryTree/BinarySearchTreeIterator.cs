0f1ea2c6e43f2f5fb36a2181fc7316edb401264e
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2024-01-07 16:59:42 +0600

    problem 72

---
+++ LeetCode150/BinaryTree/BinarySearchTreeIterator.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.LeetCode150.BinaryTree;
+
+public class BSTIterator
+{
+    private readonly Stack<TreeNode> stack = new();
+
+    public BSTIterator(TreeNode root)
+    {
+        stack.Clear();
+
+        if (root is not null)
+        {
+            stack.Push(root);
+        }
+    }
+
+    public int Next()
+    {
+        TreeNode node = stack.Pop();
+
+        if (node.right is not null)
+        {
+            stack.Push(node.right);
+        }
+
+        if (node.left is not null)
+        {
+            stack.Push(node.left);
+        }
+
+        return node.val;
+    }
+
+    public bool HasNext()
+    {
+        return stack.Count > 0;
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ LeetCode150/BinaryTree/CountCompleteTreeNodes.cs
@@ -0,0 +1,100 @@
+﻿namespace LeetCode.LeetCode150.BinaryTree;
+
+internal class CountCompleteTreeNodes
+{
+    public class Solution
+    {
+        private readonly Dictionary<TreeNode, int> leftHeights = new();
+        private readonly Dictionary<TreeNode, int> rightHeights = new();
+
+        public int CountNodes(TreeNode root)
+        {
+            leftHeights.Clear();
+            rightHeights.Clear();
+
+            return Count(root);
+        }
+
+        private int Count(TreeNode? node)
+        {
+            if (node is null)
+            {
+                return 0;
+            }
+
+            int leftHeight = GetLeftHeight(node);
+            int rightHeight = GetRightHeight(node);
+
+            if (leftHeight == rightHeight)
+            {
+                return GetCompleteTreeCount(leftHeight);
+            }
+
+            return Count(node.left) + Count(node.right) + 1;
+        }
+
+        private static int GetCompleteTreeCount(int height)
+        {
+            int count = 1;
+
+            for (int i = 0; i < height; i++)
+            {
+                count *= 2;
+            }
+
+            return count - 1;
+        }
+
+        private int GetLeftHeight(TreeNode? node)
+        {
+            if (node is null)
+            {
+                return 0;
+            }
+
+            if (leftHeights.TryGetValue(node, out int value))
+            {
+                return value;
+            }
+
+            int result =  GetLeftHeight(node.left) + 1;
+
+            leftHeights[node] = result;
+
+            return result;
+        }
+
+        private int GetRightHeight(TreeNode? node)
+        {
+            if (node is null)
+            {
+                return 0;
+            }
+
+            if (rightHeights.TryGetValue(node, out int value))
+            {
+                return value;
+            }
+
+            int result = GetRightHeight(node.right) + 1;
+
+            rightHeights[node] = result;
+
+            return result;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
