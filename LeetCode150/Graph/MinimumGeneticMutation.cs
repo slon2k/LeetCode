6133383e7cd579da0d99e96b4a1b4816ae6d6dae
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2024-01-15 09:55:42 +0600

    problems 87, 88

---
+++ LeetCode150/Graph/MinimumGeneticMutation.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.LeetCode150.Graph;
+
+internal class MinimumGeneticMutation
+{
+    public class Solution
+    {
+        public int MinMutation(string startGene, string endGene, string[] bank)
+        {
+            Queue<(string, int)> queue = new();
+            HashSet<string> visited = new();
+
+            queue.Enqueue((startGene, 0));
+
+            while (queue.Count > 0)
+            {
+                var (currentGene, step) = queue.Dequeue();
+
+                if (!visited.Contains(currentGene))
+                {
+                    visited.Add(currentGene);
+
+                    if (currentGene == endGene)
+                    {
+                        return step;
+                    }
+
+                    foreach (var gene in bank)
+                    {
+                        if (!visited.Contains(gene) && CanMutate(currentGene, gene))
+                        {
+                            queue.Enqueue((gene, step + 1));
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private bool CanMutate(string start, string end)
+        {
+            if (start.Length != end.Length)
+            {
+                return false;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < start.Length; i++)
+            {
+                if (start[i] != end[i])
+                {
+                    count++;
+                }
+            }
+
+            return count == 1;
+        }
+    }
+}
---
+++ LeetCode150/Graph/WordLadder.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.LeetCode150.Graph;
+
+internal class WordLadder
+{
+    public class Solution
+    {
+        public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+        {
+            HashSet<string> words = new(wordList);
+
+            Queue<(string, int)> queue = new();
+
+            queue.Enqueue((beginWord, 1));
+            words.Remove(beginWord);
+
+            while (queue.Count > 0)
+            {
+                var (word, count) = queue.Dequeue();
+
+                if (word == endWord)
+                {
+                    return count;
+                }
+
+                foreach (var neighbor in GetNeighbors(word, words))
+                {
+                    queue.Enqueue((neighbor, count + 1));
+                    words.Remove(neighbor);
+                }
+            }
+
+            return 0;
+        }
+
+        private IEnumerable<string> GetNeighbors(string word, HashSet<string> words)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    var neighbor = word[..i] + c + word[(i + 1)..];
+
+                    if (neighbor != word && words.Contains(neighbor))
+                    {
+                        yield return neighbor;
+                    }
+                }
+            }
+        }
+    }
+}
