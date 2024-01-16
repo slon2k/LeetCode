9e2e892add10e63a36dca80e70cee5b8bb8d070b
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2024-01-16 10:55:27 +0600

    problem 89

---
+++ LeetCode150/Math/PalindromeNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.LeetCode150.Math;
+
+internal class PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            var digits = GetDigits(x);
+
+            var reversed = digits.Reverse();
+
+            return digits.SequenceEqual(reversed);
+        }
+
+        IEnumerable<int> GetDigits(int number)
+        {
+            while (number > 0)
+            {
+                yield return number % 10;
+                number /= 10;
+            }
+        }
+    }
+}
