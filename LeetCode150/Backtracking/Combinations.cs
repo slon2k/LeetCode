05b4ef7dc08a7bcc51ea1214d531fe86fe91614b
Boris Korobeinikov <slon2k@gmail.com>
2024-01-24 12:17:30 +0600

    problems 106/150

---
+++ LeetCode150/Backtracking/Combinations.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.LeetCode150.Backtracking;
+
+public class Combinations
+{
+    public class Solution
+    {
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            Queue<(int number, IList<int> path)> queue = new();
+
+            for (int i = 1; i <= n; i++)
+            {
+                queue.Enqueue((i, new List<int>()));
+            }
+
+            while (queue.Count > 0)
+            {
+                var (number, path) = queue.Dequeue();
+
+                path.Add(number);
+
+                if (path.Count == k)
+                {
+                    result.Add(path);
+
+                    continue;
+                }
+
+                for (int i = number + 1; i <= n; i++)
+                {
+                    queue.Enqueue((i, new List<int>(path)));
+                }
+            };
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode150/Backtracking/Permutations.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.LeetCode150.Backtracking;
+
+public class Permutations
+{
+    public class Solution
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            return Permute(nums.ToList());
+        }
+
+        private IList<IList<int>> Permute(IList<int> numbers)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            if (numbers.Count == 1)
+            {
+                result.Add(new List<int>() { numbers[0] });
+
+                return result;
+            }
+            
+            foreach (int number in numbers)
+            {
+                HashSet<int> set = new(numbers);
+                
+                set.Remove(number);
+
+                var permutations = Permute(set.ToList());
+
+                foreach (var permutation in permutations)
+                {
+                    permutation.Add(number);
+                    result.Add(permutation);
+                }
+            }
+
+            return result;
+        }
+    }
+}
