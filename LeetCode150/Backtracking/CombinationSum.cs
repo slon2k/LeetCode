dc1dcaf22cfd789b417ef17e640a80921dedfe7c
Boris Korobeinikov <slon2k@gmail.com>
2024-01-26 11:29:36 +0600

    problems 109/150

---
+++ LeetCode150/Backtracking/CombinationSum.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.LeetCode150.Backtracking;
+
+public class CombinationSum
+{
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            Array.Sort(candidates);
+
+            if (candidates.Length ==  0 || target < candidates[0] )
+            {
+                return result;
+            }
+
+            int candidate = candidates[0];
+
+            if (candidate == target)
+            {
+                result.Add(new List<int>() { target });
+            }
+
+            foreach (var item in CombinationSum(candidates, target - candidate))
+            {
+                item.Add(candidate);
+                result.Add(item);
+            }
+
+            foreach (var item in CombinationSum(candidates[1..], target))
+            {
+                result.Add(item);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode150/Backtracking/GenerateParentheses.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.LeetCode150.Backtracking;
+
+public class GenerateParentheses
+{
+    public class Solution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+
+            if (n == 0)
+            {
+                return result;
+            }
+
+            Queue<string> queue = new();
+
+            queue.Enqueue("(");
+
+            while (queue.Count > 0)
+            {
+                string str = queue.Dequeue();
+
+                var left = CountChar(str, '(');
+                var right = CountChar(str, ')');
+
+                if (left == n && right == n)
+                {
+                    result.Add(str);
+                    continue;
+                }
+
+                if (left < n)
+                {
+                    queue.Enqueue(str + '(');
+                }
+
+                if (right < n && right < left)
+                {
+                    queue.Enqueue(str + ')');
+                }
+            }
+
+            return result.ToList();
+        }
+
+        private static int CountChar(string s, char c) => s.Where(x => x == c).Count();
+    }
+}
