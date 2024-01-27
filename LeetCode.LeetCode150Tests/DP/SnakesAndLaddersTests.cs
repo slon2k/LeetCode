2953dd17ffd2b82092cd0c73a91a71ff374d1a7a
Boris Korobeinikov <slon2k@gmail.com>
2024-01-27 12:50:29 +0600

    problem 110

---
+++ LeetCode.LeetCode150Tests/DP/SnakesAndLaddersTests.cs
@@ -0,0 +1,63 @@
+﻿using LeetCode.LeetCode150.Graph;
+
+namespace LeetCode.LeetCode150Tests.DP;
+
+public class SnakesAndLaddersTests
+{
+    [Theory]
+    [ClassData(typeof(SnakesAndLaddersTestData))]
+    public void SnakesAndLaddersTest(int[][] board, int expected)
+    {
+        var solution = new SnakesAndLadders.Solution();
+
+        var result = solution.SnakesAndLadders(board);
+
+        Assert.Equal(expected, result);
+    }
+
+    public class SnakesAndLaddersTestData : TheoryData<int[][], int>
+    {
+        private readonly List<List<int>> board1 = new()
+        {
+            new List<int> { -1, -1, -1, -1, -1, -1 },
+            new List<int> { -1, -1, -1, -1, -1, -1 },
+            new List<int> { -1, -1, -1, -1, -1, -1 },
+            new List<int> { -1, 35, -1, -1, 13, -1 },
+            new List<int> { -1, -1, -1, -1, -1, -1 },
+            new List<int> { -1, 15, -1, -1, -1, -1 }
+        };
+
+        private readonly List<List<int>> board2 = new()
+        {
+            new List<int> { -1, -1 },
+            new List<int> { -1, 3 },
+        };
+
+        private readonly List<List<int>> board3 = new()
+        {
+            new List<int> { 1, 1, -1 },
+            new List<int> { 1, 1, 1 },
+            new List<int> { -1, 1, 1 },
+        };
+
+        private static List<List<int>> GetBoard4()
+        {
+            var board = new List<List<int>>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                board.Add(Enumerable.Repeat(-1, 20).ToList());
+            }
+
+            return board;
+        }
+
+        public SnakesAndLaddersTestData()
+        {
+            Add(board1.Select(x => x.ToArray()).ToArray(), 4);
+            Add(board2.Select(x => x.ToArray()).ToArray(), 1);
+            Add(board3.Select(x => x.ToArray()).ToArray(), -1);
+            Add(GetBoard4().Select(x => x.ToArray()).ToArray(), 67);
+        }
+    }
+}
---
+++ LeetCode150/Graph/SnakesAndLadders.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCode.LeetCode150.Graph;
+
+public class SnakesAndLadders
+{
+    public class Solution
+    {
+        public int SnakesAndLadders(int[][] board)
+        {
+            int size = board.Length;
+
+            int target = size * size;
+
+            int[] path = Fill(board);
+
+            HashSet<int> visited = new();
+
+            Queue<(int cell, int step)> queue = new ();
+
+            queue.Enqueue((1, 0));
+
+            while (queue.Count > 0)
+            {
+                var (cell, step) = queue.Dequeue();
+
+                if (visited.Contains(cell))
+                {
+                    continue;
+                }
+
+                visited.Add(cell);
+
+                if (cell == target)
+                {
+                    return step;
+                }
+
+                for (int i = cell + 1; i <= Math.Min(cell + 6, target); i++)
+                {                  
+                    int next = path[i] == 0 ? i : path[i];
+
+                    if (next <= target && !visited.Contains(next))
+                    {
+                        queue.Enqueue((next, step + 1));
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static int[] Fill(int[][] board)
+        {
+            int size = board.Length;
+            
+            List<int> path = new() { 0 };
+
+            for (int i = 0; i < size; i++)
+            {
+                var line = new List<int>(board[size - i - 1]);
+                
+                if (i % 2 == 1)
+                {
+                    line.Reverse();
+                }
+
+                path.AddRange(line.Select(x => x == -1 ? 0 : x));
+            }
+
+            return path.ToArray();
+        }
+    }
+}
