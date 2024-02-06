8bea1c92800e815a7f31edf87e41e5137c236d20
Boris Korobeinikov <slon2k@gmail.com>
2024-02-06 14:21:02 +0600

    problem 118

---
+++ LeetCode150/DP/UniquePaths.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.LeetCode150.DP;
+
+public class UniquePaths
+{
+    public class Solution
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            int height = obstacleGrid.Length;
+            int width = obstacleGrid[0].Length;
+
+            int[,] result = new int[height + 1, width + 1];
+            result[0, 1] = 1;
+
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    if (obstacleGrid[i - 1][j - 1] == 0)
+                    {
+                        result[i, j] = result[i - 1, j] + result[i, j - 1];
+                    }
+                }
+            }
+
+            return result[height, width];
+        }
+    }
+}
