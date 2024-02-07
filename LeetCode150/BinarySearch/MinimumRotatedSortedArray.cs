8dc68f5cdc0f274dcf8db076195a294f9c895587
Boris Korobeinikov <slon2k@gmail.com>
2024-02-07 10:19:10 +0600

    problems 120/150

---
+++ LeetCode150/BinarySearch/MinimumRotatedSortedArray.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.LeetCode150.BinarySearch;
+
+internal class MinimumRotatedSortedArray
+{
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            return FindMin(nums, 0, nums.Length - 1);
+        }
+
+        private int FindMin(int[] nums, int left, int right)
+        {
+            if (left == right || nums[left] < nums[right])
+            {
+                return nums[left];
+            }
+
+            if (left == right - 1)
+            {
+                return Math.Min(nums[left], nums[right]);
+            }
+
+            int middle = left + (right - left) / 2;
+
+            if (nums[left] >= nums[middle] && nums[middle] >= nums[right]) 
+            {
+                return FindMin(nums, middle, right);
+            }
+
+            return Math.Min(FindMin(nums, left, middle), FindMin(nums, middle, right));
+        }
+    }
+}
---
+++ LeetCode150/BinarySearch/RotatedSortedArraySearch.cs
@@ -0,0 +1,71 @@
+﻿
+namespace LeetCode.LeetCode150.BinarySearch;
+
+internal class RotatedSortedArraySearch
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            return Find(target, nums, 0, nums.Length - 1);
+        }
+
+        private int Find(int target, int[] nums, int left, int right)
+        {
+            if (nums[left] == target)
+            {
+                return left;
+            }
+
+            if (nums[right] == target)
+            {
+                return right;
+            }
+
+            if (left >= right - 1)
+            {
+                return -1;
+            }
+
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target)
+            {
+                return middle;
+            }
+
+            if (nums[middle] > nums[left])
+            {
+                if (nums[middle] > target && nums[left] < target)
+                {
+                    return Find(target, nums, left, middle);
+                }
+                else
+                {
+                    return Find(target, nums, middle, right);
+                }
+            }
+
+            if (nums[middle] < nums[right])
+            {
+                if (nums[middle] < target && nums[right] > target)
+                {
+                    return Find(target, nums, middle, right);
+                }
+                else
+                {
+                    return Find(target, nums, left, middle);                    
+                }
+            }
+
+            if (nums[left] < target || nums[right] > target)
+            {
+                return Find(target, nums, left, middle);
+            }
+            else
+            {
+                return Find(target, nums, middle, right);
+            }
+        }
+    }
+}
