cfab13aabc57240e8690db8521e37728e7abab9c
Boris Korobeinikov <slon2k@gmail.com>
2024-02-09 09:07:26 +0600

    problems 122/150

---
+++ LeetCode.LeetCode150Tests/DP/InterleavingStringTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.LeetCode150.DP;
+
+namespace LeetCode.LeetCode150Tests.DP;
+
+public class InterleavingStringTests
+{
+    [Theory]
+    [InlineData("aabcc", "dbbca", "aadbbcbcac", true)]
+    [InlineData("aabcc", "dbbca", "aadbbbaccc", false)]
+    [InlineData("aaaa", "aa", "aaa", false)]
+    [InlineData(
+        "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
+        "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab",
+        "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab",
+        false
+        )]
+    [InlineData("", "", "", true)]
+    public void IsInterleave(string s1, string s2, string s3, bool expected)
+    {
+        var solution = new InterleavingString.Solution();
+
+        var result = solution.IsInterleave(s1, s2, s3);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode150/DP/InterleavingString.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode.LeetCode150.DP;
+
+public class InterleavingString
+{
+    public class Solution
+    {
+        private readonly Dictionary<(string, string, string), bool> results = new();
+
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            return IsInterleaving(s1, s2, s3);
+        }
+
+        private bool IsInterleaving(string s1, string s2, string s3)
+        {
+            if (results.ContainsKey((s1, s2, s3)))
+            {
+                return results[(s1, s2, s3)];
+            }
+
+            if (s3.Length != s1.Length + s2.Length)
+            {
+                return false;
+            }
+
+            if (s1 == "")
+            {
+                return s2 == s3;
+            }
+
+            if (s2 == "")
+            {
+                return s1 == s3;
+            }
+
+            if (s1[0] != s3[0] && s2[0] != s3[0])
+            {
+                return false;
+            }
+
+            if (s1[0] == s3[0] && s2[0] != s3[0])
+            {
+                results[(s1, s2, s3)] = IsInterleaving(s1[1..], s2, s3[1..]);
+                return results[(s1, s2, s3)];
+            }
+
+            if (s2[0] == s3[0] && s1[0] != s3[0])
+            {
+                results[(s1, s2, s3)] = IsInterleaving(s1, s2[1..], s3[1..]);
+                return results[(s1, s2, s3)];
+            }
+
+            if (IsInterleaving(s1[1..], s2, s3[1..]))
+            {
+                results[(s1, s2, s3)] = true;
+                return results[(s1, s2, s3)];
+            }
+
+            if (IsInterleaving(s1, s2[1..], s3[1..]))
+            {
+                results[(s1, s2, s3)] = true;
+                return results[(s1, s2, s3)];
+            }
+
+            return false;
+        }
+    }
+}
