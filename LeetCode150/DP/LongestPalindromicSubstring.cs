26a4faf3be8b5e2e0e3dbb1948c0987d4666d13c
Boris Korobeinikov <slon2k@gmail.com>
2024-05-27 19:32:05 +0500

    problem 126

---
+++ LeetCode150/DP/LongestPalindromicSubstring.cs
@@ -0,0 +1,80 @@
+﻿namespace LeetCode.LeetCode150.DP;
+
+public class LongestPalindromicSubstring
+{
+    public class Solution
+    {
+        private readonly HashSet<string> palindromes = new();
+
+        private readonly Dictionary<string, string> results = new();
+
+        public string LongestPalindrome(string s)      
+        {
+            palindromes.Clear();
+            results.Clear();
+
+            return FindPalindrome(s);
+        }
+
+
+        private string FindPalindrome(string s)
+        {
+            if(IsPalindrome(s))
+            {
+                results[s] = s;
+                return s;
+            }
+
+            if(results.ContainsKey(s))
+            {
+                return results[s];
+            }
+
+
+            string leftSubstring = s[..^1];
+            string left = FindPalindrome(leftSubstring);
+
+            string rightSubstring = s[1..];
+            string right = FindPalindrome(rightSubstring);
+
+            if(left.Length >= right.Length)
+            {
+                results[s] = left;
+                return left;
+            }
+            else
+            {
+                results[s] = right;
+                return right;
+            }
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            if (s.Length <= 1)
+            {
+                return true;
+            }
+
+            if (s[0] != s[^1])
+            {
+                return false;
+            }
+
+            if (palindromes.Contains(s))
+            {
+                return true;
+            }
+
+            string substring = s[1..^1];
+            bool result = IsPalindrome(substring);
+
+            if (result)
+            {
+                palindromes.Add(s);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode150/DP/Triangle.cs
@@ -0,0 +1,42 @@
+﻿
+namespace LeetCode.LeetCode150.DP;
+
+public class Triangle
+{
+    public class Solution
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            List<List<int>> results = [];
+
+            int height = triangle.Count;
+
+            results.Add([triangle[0][0]]);
+
+            for (int i = 1; i < height; i++)
+            {
+                results.Add([]);
+
+                for (int j = 0; j < triangle[i].Count; j++)
+                {
+                    int current;
+                    
+                    if (j == 0)
+                    {
+                        current = results[i - 1][0];
+                    } else if (j == triangle[i].Count - 1)
+                    {
+                        current = results[i - 1][j - 1];
+                    } else
+                    {
+                        current = Math.Min(results[i - 1][j], results[i - 1][j - 1]);
+                    }
+
+                    results[i].Add(current + triangle[i][j]);
+                }
+            }
+
+            return results[height - 1].Min();
+        }
+    }
+}
