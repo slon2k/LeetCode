d1d5ef6b36a4cb25b4e5b59b397447d00299440c
Boris <slon2k@gmail.com>
2026-03-17 13:01:28 +0500

    Add solution for Count Operations to Obtain Zero problem

---
+++ LeetCode2026/Mathematics/CountOperationsToObtainZero.cs
@@ -0,0 +1,25 @@
+namespace LeetCode2026.Mathematics;
+
+public class CountOperationsToObtainZero
+{
+    public int CountOperations(int num1, int num2)
+    {
+        var count = 0;
+        
+        while (num1 != 0 && num2 != 0)
+        {
+            if (num1 >= num2)
+            {
+                num1 -= num2;
+            }
+            else
+            {
+                num2 -= num1;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
